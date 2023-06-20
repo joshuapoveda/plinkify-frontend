55663167cc885795e3a76218dc2fab5536b16b63
Joshua Poveda <joshuapoveda9@gmail.com>
2023-06-20 15:16:00 -0400

    refresh bug pt43

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
             />
             <Route
               path="/main"
-              element={<Landing/>}
+              element={!user ? <Navigate to="/" />: <Home/>}
             />
           </Routes>
         </div>
